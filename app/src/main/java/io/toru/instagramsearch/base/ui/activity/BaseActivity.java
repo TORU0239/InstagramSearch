996f36fc92216b8f6045c6a2caec9e6b50f348d5
Toru Choi <blueocean239@gmail.com>
2017-05-18 17:06:12 +0900

    basic toolbar theme set, base abstract activty setting

---
+++ app/src/main/java/io/toru/instagramsearch/base/ui/activity/BaseActivity.java
@@ -0,0 +1,46 @@
+package io.toru.instagramsearch.base.ui.activity;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import io.toru.instagramsearch.R;
+
+/**
+ * Created by wonyoung on 2017. 5. 18..
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+    protected ViewDataBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(getCurrentActivity(), getLayoutId());
+        setAppTaskDescription();
+        initUI();
+    }
+
+    @TargetApi(21)
+    protected void setAppTaskDescription() {
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+            Bitmap bm = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+            ActivityManager.TaskDescription taskDescription = new ActivityManager.TaskDescription(getString(R.string.app_name), bm,
+                    ContextCompat.getColor(this, R.color.gelato_color_primary_dark));
+            setTaskDescription(taskDescription);
+        }
+    }
+
+    public abstract Activity getCurrentActivity();
+    public abstract int getLayoutId();
+    public abstract void initUI();
+}
---
+++ app/src/main/java/io/toru/instagramsearch/view/MainActivity.java
@@ -0,0 +1,27 @@
+package io.toru.instagramsearch.view;
+
+import android.app.Activity;
+
+import io.toru.instagramsearch.R;
+import io.toru.instagramsearch.base.ui.activity.BaseActivity;
+import io.toru.instagramsearch.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+    private ActivityMainBinding mainBinding;
+
+    @Override
+    public Activity getCurrentActivity() {
+        return MainActivity.this;
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initUI() {
+        mainBinding = (ActivityMainBinding)binding;
+        mainBinding.txtTest.setText("hello, world!!");
+    }
+}

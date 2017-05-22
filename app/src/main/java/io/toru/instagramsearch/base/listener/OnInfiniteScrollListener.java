635f947f5cb08b1afd0dc000285d50a127e49ec2
Toru Choi <blueocean239@gmail.com>
2017-05-22 13:43:12 +0900

    uncommited file

---
+++ app/src/main/java/io/toru/instagramsearch/base/listener/OnInfiniteScrollListener.java
@@ -0,0 +1,9 @@
+package io.toru.instagramsearch.base.listener;
+
+/**
+ * Created by wonyoung on 2017. 5. 22..
+ */
+
+public interface OnInfiniteScrollListener {
+    void onLoadMore(String lastImageId);
+}

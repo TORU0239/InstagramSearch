6a6b5f009f105b0dfde65f3bb2f19eeb2df82500
Toru Choi <blueocean239@gmail.com>
2017-05-23 13:13:10 +0900

    Add fixed Toast to MainPresenterImp for showing it using string id

--- app/src/main/java/io/toru/instagramsearch/main/presenter/MainPresenterImpl.java
+++ app/src/main/java/io/toru/instagramsearch/main/presenter/MainPresenterImpl.java
@@ -110,13 +110,13 @@ public class MainPresenterImpl implements MainTask.MainPresenter {
                     }
                     catch (NullPointerException e){
                         e.printStackTrace();
-                        mainView.onShowToast("관련된 데이터가 없습니다.");
+                        mainView.onShowToast(R.string.no_data);
                     }
                 }
                 else{
                     Log.w(TAG, "failed! " + response.code());
                     mainView.onHideProgressDialog();
-                    mainView.onShowToast("서버에서 응답을 받아오는 데 실패했습니다.");
+                    mainView.onShowToast(R.string.failed_to_server_response);
                 }
             }
 
